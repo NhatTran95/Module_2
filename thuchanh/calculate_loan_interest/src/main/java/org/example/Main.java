feed42fade36b6b83b21dde5f4c9bebf78d5ab8a
NhatTran95 <tranducanhnhat@gmail.com>
2023-05-10 16:27:02 +0700

    up

---
+++ thuchanh/calculate_loan_interest/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số tiền cho vay: ");
+        double money = scanner.nextDouble();
+        System.out.println("Nhập lãi suất: ");
+        double interestRate = scanner.nextDouble();
+        System.out.println("Nhập số tháng vay: ");
+        int month = scanner.nextInt();
+        double totalIntersest = money * (interestRate/100)/12 * month;
+        double totalMoney = money + totalIntersest;
+        System.out.printf("Tổng số tiền phải trả khi vay %.2f triệu với lãi suất %.2f phần trăm trên năm trong %s tháng là %.2f triệu", money, interestRate, month, totalMoney);
+    }
+}
