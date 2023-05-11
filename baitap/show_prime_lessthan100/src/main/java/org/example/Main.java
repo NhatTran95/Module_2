b99b008a769f1398eff86ee5c964de12550f6970
NhatTran95 <tranducanhnhat@gmail.com>
2023-05-11 16:48:14 +0700

    up

---
+++ baitap/show_prime_lessthan100/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int N = 2;
+        while (N < 100) {
+            if (isPrime(N)) {
+                System.out.println(N);
+            }
+            N++;
+        }
+    }
+    public static boolean isPrime(int num) {
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
