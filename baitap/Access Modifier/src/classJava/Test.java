915bd84e932eef4a38b4c3a6c2fcf6dd977db4f7
NhatTran95 <tranducanhnhat@gmail.com>
2023-05-17 22:11:59 +0700

    access

---
+++ baitap/Access Modifier/src/classJava/Test.java
@@ -0,0 +1,9 @@
+package classJava;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("nhat");
+        student.setClasses("C03");
+    }
+}
