ada9d5f9495ac28f29a3c2a56ffb77aa5156b7d3
NhatTran95 <tranducanhnhat@gmail.com>
2023-05-25 13:38:43 +0700

    up

---
+++ JavaExercise/src/exercise/refactoring/SimpleCaculator.java
@@ -0,0 +1,11 @@
+package exercise.refactoring;
+
+public class SimpleCaculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
